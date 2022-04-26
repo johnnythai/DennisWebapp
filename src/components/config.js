34058744417dd6fdaf8e3603c239a0edc3f052b0
Johnny Thai <jthai04@gmail.com>
2022-04-25 17:41:54 -0700

    config

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
-const apiURL = 'https://johnnythai.dev'
-const wsURL = 'wss://johnnythai.dev'
+const apiURL = 'https://alpaca.johnnythai.dev'
+const wsURL = 'wss://alpaca.johnnythai.dev'
 
 // const apiURL = 'http://localhost:8000'
 // const wsURL = 'ws://localhost:8000'
